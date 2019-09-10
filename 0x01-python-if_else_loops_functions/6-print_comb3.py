b05cef55103ae4fde6dec2594b107bc05230f966
Anthony <huggins9000211@gmail.com>
2019-09-10 12:55:37 -0400

    test

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+i = 0
+i2 = 1
+i3 = 1
+while i < 9:
+    while i2 <= 8:
+        print("{}{}".format(i, i2), end=", ")
+        i2 = i2 + 1
+    i2 = i3 + 1
+    i3 = i2
+    i = i + 1
+print("89")
