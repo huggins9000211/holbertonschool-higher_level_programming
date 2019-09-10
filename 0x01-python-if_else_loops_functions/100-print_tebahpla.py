d01e8fcdba684636920ae57cbf73bd936521a396
Anthony <huggins9000211@gmail.com>
2019-09-10 17:15:41 -0400

    test

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,9 +3,9 @@ capital = False
 index = 122
 while (index >= 97):
     if capital:
-        print(chr(index - 32), end=" ")
+        print(chr(index - 32), end="")
         capital = False
     else:
-        print(chr(index), end=" ")
+        print(chr(index), end="")
         capital = True
     index = index - 1
