2fbe8f3d80764f8fc6b9b89e3408c103409964df
Anthony <huggins9000211@gmail.com>
2019-09-09 22:53:05 -0400

    test

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+    listint_x *myList;
+    listint_t *current;
+    unsigned int n; /* number of nodes */
+
+    current = list;
+    n = 0;
+    while (current != NULL)
+    {
+        if (n == 0)
+        {
+            *add_nodeList(&myList, current);
+        }
+        else if (checkMyList(myList, current))
+        {
+            return (1);
+        }
+
+        //printf("%i\n", current->currentt);
+        current = current->next;
+        n++;
+    }
+}
+
+int checkMyList(listint_x *head, listint_t *current)
+{
+    const listint_t *currentz;
+    const listint_x *myList;
+    myList = head;
+    currentz = current;
+
+    while (myList != NULL)
+    {
+        if (currentz == myList->currentt) {
+            return (1);
+        }
+
+        myList = myList->next;
+    }
+    return (0);
+    
+}
---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,33 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+typedef struct listint_y
+{
+    listint_t *currentt;
+    struct listint_y *next;
+} listint_x;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
+listint_x *add_nodeList(listint_x **head, listint_t *n);
+void free_listList(listint_x *head);
+int checkMyList(listint_x *head, listint_t *current);
+#endif /* LISTS_H */
