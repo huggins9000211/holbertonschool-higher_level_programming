e090722cc86f5b2a2c32542efbca02f95a647704
Anthony <huggins9000211@gmail.com>
2019-09-12 17:39:19 -0400

    test

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,62 +1,10 @@
 #!/usr/bin/python3
 import sys
+from calculator_1 import add, sub, mul, div
 arggs = sys.argv
 opp = arggs[2]
 num1 = int(arggs[1])
 num2 = int(arggs[3])
-
-
-def add(a, b):
-    """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
-
-
-def sub(a, b):
-    """My subtraction function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a - b
-    """
-    return (a - b)
-
-
-def mul(a, b):
-    """My multiplication function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a * b
-    """
-    return (a * b)
-
-
-def div(a, b):
-    """My division function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a / b
-    """
-    return int(a / b)
-
 if len(arggs) != 4:
     print('Usage: ./100-my_calculator.py <a> <operator> <b>')
     exit(1)
