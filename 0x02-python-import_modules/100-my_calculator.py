f4d0219df5a934ba02a83cd9884365c93f98d9b7
Anthony <huggins9000211@gmail.com>
2019-09-12 17:43:18 -0400

    test

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
-arggs = sys.argv
-if len(arggs) != 4:
-    print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-    exit(1)
-opp = arggs[2]
-num1 = int(arggs[1])
-num2 = int(arggs[3])
-if opp == '+':
-    temp = add(num1, num2)
-    print("{} {} {} = {}".format(num1, opp, num2, temp))
-elif opp == '-':
-    temp = sub(num1, num2)
-    print("{} {} {} = {}".format(num1, opp, num2, temp))
-elif opp == '*':
-    temp = mul(num1, num2)
-    print("{} {} {} = {}".format(num1, opp, num2, temp))
-elif opp == '/':
-    temp = div(num1, num2)
-    print("{} {} {} = {}".format(num1, opp, num2, temp))
-else:
-    print('Unknown operator. Available operators: +, -, * and /')
-    exit(1)
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    arggs = sys.argv
+    if len(arggs) != 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+    opp = arggs[2]
+    num1 = int(arggs[1])
+    num2 = int(arggs[3])
+    if opp == '+':
+        temp = add(num1, num2)
+        print("{} {} {} = {}".format(num1, opp, num2, temp))
+    elif opp == '-':
+        temp = sub(num1, num2)
+        print("{} {} {} = {}".format(num1, opp, num2, temp))
+    elif opp == '*':
+        temp = mul(num1, num2)
+        print("{} {} {} = {}".format(num1, opp, num2, temp))
+    elif opp == '/':
+        temp = div(num1, num2)
+        print("{} {} {} = {}".format(num1, opp, num2, temp))
+    else:
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
