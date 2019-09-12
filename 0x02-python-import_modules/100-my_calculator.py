f29510c95cad54c450453237cf99512e10bfde9a
Anthony <huggins9000211@gmail.com>
2019-09-12 17:41:20 -0400

    test

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,12 +2,12 @@
 import sys
 from calculator_1 import add, sub, mul, div
 arggs = sys.argv
-opp = arggs[2]
-num1 = int(arggs[1])
-num2 = int(arggs[3])
 if len(arggs) != 4:
     print('Usage: ./100-my_calculator.py <a> <operator> <b>')
     exit(1)
+opp = arggs[2]
+num1 = int(arggs[1])
+num2 = int(arggs[3])
 if opp == '+':
     temp = add(num1, num2)
     print("{} {} {} = {}".format(num1, opp, num2, temp))
