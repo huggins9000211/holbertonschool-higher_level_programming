7c45225d26ac5f47b9b849b6223b4e6e7255688f
Anthony <huggins9000211@gmail.com>
2019-09-12 21:30:53 -0400

    test

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,10 @@ import sys
 if __name__ == "__main__":
     arggs = sys.argv
     i = 1
-    print("{} arguments:".format(len(arggs) - 1))
+    if len(arggs) == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len(arggs) - 1))
     for x in range(1, len(arggs)):
         print("{}: {}".format(i, arggs[x]))
         i += 1
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4 as x
+    import hidden_4 as 
     print(x)
-    
