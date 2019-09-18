dee5b20b280fbd2a50f770fc6777d8ef1bde8ad9
Anthony <huggins9000211@gmail.com>
2019-09-17 20:59:07 -0400

    test

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for x in matrix:
+        for y in matrix:
+            print("{}".format(y), end="")
+
+        print("{}")
+        pass
+    
