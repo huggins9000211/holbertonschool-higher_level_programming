bc62a198532064c403b65ef2a0ace6b1fc8cee09
Anthony <huggins9000211@gmail.com>
2019-12-10 13:24:59 -0500

    test

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,5 +7,5 @@ if numString > 5:
     print("and is greater than 5")
 elif numString == 0:
     print("and is 0")
-elif numString < 6 and numString != 0:
+elif numString < 6:
     print("and is less than 6 and not 0")
