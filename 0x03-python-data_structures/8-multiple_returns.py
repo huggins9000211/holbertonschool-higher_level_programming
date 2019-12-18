fcad82ecee3f2c76f24970e82ef5a7c2087705e0
Anthony <huggins9000211@gmail.com>
2019-12-18 12:32:17 -0500

    test

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == "":
-        return None
-    length = len(sentence)
-    char = sentence[0]
-    return length, char
+    if sentence:
+        length = len(sentence)
+        char = sentence[0]
+        return length, char
