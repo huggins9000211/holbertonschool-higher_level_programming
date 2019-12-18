d4a21de35142aef4b54ebe8a643b907506fe03c5
Anthony <huggins9000211@gmail.com>
2019-12-18 12:19:11 -0500

    test

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    print(x)
+    my_list = dir(hidden_4)
+    for x in my_list:
+        if x[1] != '_':
+            print(x)
--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * is_palindrome - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 int is_palindrome(listint_t **head)
 {
-
+    listint_t
 }
