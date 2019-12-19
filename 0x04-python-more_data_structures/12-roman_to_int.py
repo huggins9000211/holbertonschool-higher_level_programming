a52e03be4d7aa460d37dbf593b7494a2252a1c57
Anthony <huggins9000211@gmail.com>
2019-12-19 15:26:31 -0500

    test

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -29,7 +29,7 @@ def roman_to_int(roman_string):
         else:
             res = res + s1
             i = i + 1
-    if type(roman_string) != str or roman_string == None:
+    if type(roman_string) != str or roman_string is None:
         return 0
     else:
         return res
