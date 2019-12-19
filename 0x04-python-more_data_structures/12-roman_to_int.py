0c4f88a4e1dddf299e0a085a1613321b68e5cc1d
Anthony <huggins9000211@gmail.com>
2019-12-19 15:25:34 -0500

    test

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,52 +2,52 @@
 def roman_to_int(roman_string):
     res = 0
     i = 0
-    
 
-    while (i < len(roman_string)): 
-        
-        # Getting value of symbol s[i] 
-        s1 = value(roman_string[i]) 
-  
-        if (i+1 < len(roman_string)): 
-  
-            # Getting value of symbol s[i+1] 
-            s2 = value(roman_string[i+1]) 
-  
-            # Comparing both values 
-            if (s1 >= s2): 
-  
-                # Value of current symbol is greater 
-                # or equal to the next symbol 
-                res = res + s1 
+    while (i < len(roman_string)):
+
+        # Getting value of symbol s[i]
+        s1 = value(roman_string[i])
+
+        if (i+1 < len(roman_string)):
+
+            # Getting value of symbol s[i+1]
+            s2 = value(roman_string[i+1])
+
+            # Comparing both values
+            if (s1 >= s2):
+
+                # Value of current symbol is greater
+                # or equal to the next symbol
+                res = res + s1
                 i = i + 1
-            else: 
-  
-                # Value of current symbol is greater 
-                # or equal to the next symbol 
-                res = res + s2 - s1 
+            else:
+
+                # Value of current symbol is greater
+                # or equal to the next symbol
+                res = res + s2 - s1
                 i = i + 2
-        else: 
-            res = res + s1 
+        else:
+            res = res + s1
             i = i + 1
     if type(roman_string) != str or roman_string == None:
         return 0
     else:
         return res
 
-def value(r): 
-    if (r == 'I'): 
+
+def value(r):
+    if (r == 'I'):
         return 1
-    if (r == 'V'): 
+    if (r == 'V'):
         return 5
-    if (r == 'X'): 
+    if (r == 'X'):
         return 10
-    if (r == 'L'): 
+    if (r == 'L'):
         return 50
-    if (r == 'C'): 
+    if (r == 'C'):
         return 100
-    if (r == 'D'): 
+    if (r == 'D'):
         return 500
-    if (r == 'M'): 
+    if (r == 'M'):
         return 1000
     return -1
