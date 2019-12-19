f6af0564230301e29e6978bc402bf36a86fa7954
Anthony <huggins9000211@gmail.com>
2019-12-19 14:46:38 -0500

    test

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    best = (None, -float("inf"))
+    if a_dictionary:
+        for x, y in a_dictionary.items():
+            if y > best[1]:
+                best = (x, y)
+    if best[1] == -float("inf"):
+        return None
+    return(best[1])
