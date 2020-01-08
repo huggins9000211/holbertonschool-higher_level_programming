2457be233aaa0ca82ad4d0e59d116914b5bdd7a2
Anthony <huggins9000211@gmail.com>
2020-01-08 12:13:34 -0500

    test

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    counter = 0
+    for x in range(0, x):
+        try:
+            print("{:d}".format(my_list[x]), end="")
+        except:
+            pass
+        else:
+            counter += 1
+    print()
+    return counter
