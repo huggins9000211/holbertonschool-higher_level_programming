22b29f654a9a75e2de2718002c7086c2e3b36e1c
Anthony <huggins9000211@gmail.com>
2020-01-08 12:05:31 -0500

    test

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[x], end="")
         except IndexError as err:
             pass
-		else:
+        else:
             counter += 1
     print()
     return counter
