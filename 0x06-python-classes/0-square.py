e83277b6fb845570920a170945d39b98721fc063
Anthony <huggins9000211@gmail.com>
2020-01-09 13:05:35 -0500

    test

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+class Square:
+     dic = {}
+     name = "no name"
+     __password = None
+     
+     def __init__(self, size=None):
+         self.__size = size
+         if new_name is not None:
+             self.name = new_name
+ 
