0d0455b36b3d9fae77c452e4737c5dd6c40cab58
Anthony <huggins9000211@gmail.com>
2020-01-09 19:10:04 -0500

    test

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,8 +1,9 @@
+#!/usr/bin/python3
 class Node:
     def __init__(self, data, next_node=None):
         self.next_node = next_node
         self.data = data
-    
+
     @property
     def data(self):
         return self.__data
@@ -25,39 +26,39 @@ class Node:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
+
 class SinglyLinkedList:
     def __init__(self):
         self.__head = None
 
     def sorted_insert(self, value):
         new_node = Node(value)
-        if self.__head is None: 
-            new_node.next_node = self.__head 
-            self.__head = new_node 
-  
-        # Special case for __head at end 
-        elif self.__head.data >= new_node.data: 
-            new_node.next_node = self.__head 
-            self.__head = new_node 
-  
-        else : 
-  
-            # Locate the node before the point of insertion 
-            current = self.__head 
+        if self.__head is None:
+            new_node.next_node = self.__head
+            self.__head = new_node
+
+        # Special case for __head at end
+        elif self.__head.data >= new_node.data:
+            new_node.next_node = self.__head
+            self.__head = new_node
+
+        else:
+
+            # Locate the node before the point of insertion
+            current = self.__head
             while(current.next_node is not None and
-                 current.next_node.data < new_node.data): 
+                  current.next_node.data < new_node.data):
                 current = current.next_node
-              
+
             new_node.next_node = current.next_node
-            current.next_node = new_node 
-  
+            current.next_node = new_node
+
     def __str__(self):
         temp = self.__head
 
         if not temp:
             return ''
-        while not (temp.next_node is None): 
-            print(temp.data) 
+        while not (temp.next_node is None):
+            print(temp.data)
             temp = temp.next_node
         return str(temp.data)
-
