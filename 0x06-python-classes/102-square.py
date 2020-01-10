ec3e4ff387ce37370b86b054cbfaeda9d97d5c9f
Anthony <huggins9000211@gmail.com>
2020-01-09 19:35:02 -0500

    test

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+
+    def area(self):
+        return self.size * self.size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not (isinstance(value, int) or isinstance(value, float)):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def __lt__(self, y):
+        return self.area() < y.area()
+    def __le__(self, y):
+        return self.area() <= y.area()
+    def __eq__(self, y):
+        return self.area() == y.area()
+    def __ne__(self, y):
+        return self.area() != y.area()
+    def __gt__(self, y):
+        return self.area() > y.area()
+    def __ge__(self, y):
+        return self.area() >= y.area()
