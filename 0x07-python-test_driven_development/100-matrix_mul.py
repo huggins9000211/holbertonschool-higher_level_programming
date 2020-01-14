b5f947ebcff7feb7a040578d4b5018df23daa555
Anthony <huggins9000211@gmail.com>
2020-01-14 00:13:03 -0500

    test

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -6,3 +6,7 @@
 
 def matrix_mul(m_a, m_b):
     """ text indentation """
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+    lazy matrix
+"""
+import numpy
+
+def lazy_matrix_mul(m_a, m_b):
+    """ lazy matrix """
+    return numpy.dot(m_a, m_b)
