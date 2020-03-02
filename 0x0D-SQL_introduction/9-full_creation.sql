b2136e7826a4eb1adfd931a2d5404bb43d449628
Anthony <huggins9000211@gmail.com>
2020-03-02 14:38:37 -0500

    test

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,15 @@
+-- Show databases
+-- Show databases
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+)
+-- Show databases
+INSERT INTO first_table VALUES ('1', 'John', '10');
+-- Show databases
+INSERT INTO first_table VALUES ('2', 'Alex', '3');
+-- Show databases
+INSERT INTO first_table VALUES ('3', 'Bob', '14');
+-- Show databases
+INSERT INTO first_table VALUES ('4', 'George', '8');
