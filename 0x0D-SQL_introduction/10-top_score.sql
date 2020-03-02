fdb3dff5c0d266ed088b27facc7e8333af94e424
Anthony <huggins9000211@gmail.com>
2020-03-02 15:21:51 -0500

    test

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- Show databases
 -- Show databases
-SELECT score, name FROM second_table WHERE name ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
