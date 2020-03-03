355e774e45bff7ff56638f5670664d25d2011724
Anthony <huggins9000211@gmail.com>
2020-03-03 13:22:42 -0500

    test

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- Show databases
+-- Show databases
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Show databases
+USE hbtn_0d_usa
+-- Show databases
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+)
