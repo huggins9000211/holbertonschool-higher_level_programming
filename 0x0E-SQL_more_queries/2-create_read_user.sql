c4270bef34c39f250af62046edf1cbdbbb32f765
Anthony <huggins9000211@gmail.com>
2020-03-03 13:11:44 -0500

    test

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,4 +4,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Show databases
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 -- Show databases
-GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
