de183200db73ee7ecc144b75d101e75f4575ef7f
Anthony <huggins9000211@gmail.com>
2020-03-03 13:34:35 -0500

    test

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -8,5 +8,5 @@ CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT NOT NULL UNIQUE PRIMARY KEY,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES cities (id)
+    FOREIGN KEY (state_id) REFERENCES states (id)
 )
