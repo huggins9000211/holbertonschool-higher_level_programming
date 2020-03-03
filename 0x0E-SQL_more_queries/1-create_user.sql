45edc32df13c54da25d333a49498d0ddaec3045f
Anthony <huggins9000211@gmail.com>
2020-03-03 13:08:18 -0500

    test

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,7 @@
 -- Show databases
 -- Show databases
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Show databases
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- Show databases
+GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
