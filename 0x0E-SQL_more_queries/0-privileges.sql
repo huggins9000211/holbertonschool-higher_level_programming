4da5bcec2a9f502f18a3f072c8a575aa411fcf7a
Anthony <huggins9000211@gmail.com>
2020-03-04 10:02:49 -0500

    test

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- Show databases
 -- Show databases
-SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 -- Show databases
-SHOW GRANTS FOR user_0d_2@localhost;
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
