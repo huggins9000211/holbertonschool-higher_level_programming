9e7cb4585bc1fd195c12ff0d36042e3c14f12c2d
Anthony <huggins9000211@gmail.com>
2020-03-29 18:30:23 -0400

    test

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -3,6 +3,7 @@
 if __name__ == "__main__":
     import MySQLdb
     import sys
+    myList = []
     db = MySQLdb.connect(
         user=sys.argv[1], password=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
@@ -10,6 +11,7 @@ if __name__ == "__main__":
     INNER JOIN states s ON c.state_id = s.id
     WHERE s.name = %s ORDER BY c.id""", (sys.argv[4],))
     states = c.fetchall()
-    for x in states[:-1]:
-        print(x[1], end=', ')
-    print(states[-1][1])
+    for x in states:
+        if x[2] == sys.argv[4]:
+            myList.append(x[1])
+    print(', '.join(myList))
