c71fa94bbcf52aa00090292fc000a889e6af0c90
Anthony <huggins9000211@gmail.com>
2020-04-07 22:46:10 -0400

    test

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,15 +3,15 @@
 
 
 def findPeakUtil(arr, low, high, n):
-    mid = low + (high - low)/2 
+    mid = low + (high - low)/2
     mid = int(mid)
 
     if ((mid == 0 or arr[mid - 1] <= arr[mid]) and
-     (mid == n - 1 or arr[mid + 1] <= arr[mid])): 
+       (mid == n - 1 or arr[mid + 1] <= arr[mid])):
         return mid
-    elif (mid > 0 and arr[mid - 1] > arr[mid]): 
+    elif (mid > 0 and arr[mid - 1] > arr[mid]):
         return findPeakUtil(arr, low, (mid - 1), n)
-    else: 
+    else:
         return findPeakUtil(arr, (mid + 1), high, n)
 
 
