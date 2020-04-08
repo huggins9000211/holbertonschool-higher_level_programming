0854bb48fbf4fd8deddef69ab72bf6176e02b4f3
Anthony <huggins9000211@gmail.com>
2020-04-07 22:25:37 -0400

    test

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #test
-curl -s -X POST -H "email: hr@holbertonschool.com" -H "subject: I will always be here for PLD" "$1"
+curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
