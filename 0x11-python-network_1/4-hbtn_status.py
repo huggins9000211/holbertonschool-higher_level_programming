b8b527daf34554daa76dc85d86bba9dde6f23067
Anthony <huggins9000211@gmail.com>
2020-04-13 22:33:53 -0400

    test

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" test """
+if __name__ == "__main__":
+    import requests
+    x = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t - type: {}'.format(type(x.text)))
+    print('\t - content: {}'.format(x.text))
