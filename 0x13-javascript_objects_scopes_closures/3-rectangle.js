0e9b87d8284b17f3d7c69b3fd2dd32336e413806
Anthony <huggins9000211@gmail.com>
2020-05-11 16:21:24 -0400

    test

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+      const x = "".padStart(this.width, 'X');
+      let y = 0;
+      while (y < this.height) {
+          console.log(x);
+          y++;
+      }
+  }
+};
