08cec93f91f870a7bee315a6aa853109a5db5df5
Anthony <huggins9000211@gmail.com>
2020-05-11 16:37:51 -0400

    test

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    const x = ''.padStart(this.width, c);
+    let y = 0;
+    while (y < this.height) {
+      console.log(x);
+      y++;
+    }
+  }
+};
