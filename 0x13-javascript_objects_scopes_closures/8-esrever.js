5463da3d0ee40ffa5997198bdf5228b0fe7b473c
Anthony <huggins9000211@gmail.com>
2020-05-11 17:07:27 -0400

    test

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const result = [];
-  for (let x = list.length - 1; x >= 0; x++) {
+  for (let x = list.length - 1; x >= 0; x--) {
     result.push(list[x]);
   }
   return result;
