48d3fd36dcf61fccf88cdbc66a9eafc4bbd7a2b5
Anthony <huggins9000211@gmail.com>
2020-05-12 15:15:08 -0400

    test

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+  let count = 0;
+  if (error) {
+    // console.error('error:', error);
+  }
+  // console.log('code:', response && response.statusCode);
+  const movies = JSON.parse(body).results;
+  let x;
+  for (x of movies) {
+    if (x.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) count++;
+  }
+  console.log(count);
+});
