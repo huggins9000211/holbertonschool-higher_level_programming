6dcfb315731e3ce4c6dc5ce0c35bd5c0be2c2239
Anthony <huggins9000211@gmail.com>
2020-05-12 14:05:37 -0400

    test

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,10 +2,9 @@
 const Square1 = require('./5-square.js');
 module.exports = class Square extends Square1 {
   charPrint (c = 'X') {
-    const x = ''.padStart(this.width, c);
     let y = 0;
     while (y < this.height) {
-      console.log(x);
+      console.log(c);
       y++;
     }
   }
