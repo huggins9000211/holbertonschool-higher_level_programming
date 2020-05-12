836049dbf27795a05c04315e0b898777a00e06fe
Anthony <huggins9000211@gmail.com>
2020-05-12 14:38:23 -0400

    test

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,6 @@ const request = require('request');
 
 request(process.argv[2], function (error, response, body) {
   if (error) console.error('error:', error);
-  console.log('statusCode:', response && response.statusCode);
+  console.log('code:', response && response.statusCode);
   // console.log('body:', body);
 });
