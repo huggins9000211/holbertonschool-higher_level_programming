078901655a3493d49e66ca00579f7024b812018f
Anthony <huggins9000211@gmail.com>
2020-05-12 14:50:55 -0400

    test

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const request = require('request');
+
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, function (error, response, body) {
+  if (error) console.error('error:', error);
+  // console.log('code:', response && response.statusCode);
+  console.log(JSON.parse(body).title);
+});
