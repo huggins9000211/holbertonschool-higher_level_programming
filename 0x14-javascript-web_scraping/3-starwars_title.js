1394db55d02f90fbcdc880317c9919db2c413feb
Anthony <huggins9000211@gmail.com>
2020-05-12 14:53:21 -0400

    test

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,9 @@
 const request = require('request');
 
 request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, function (error, response, body) {
-  if (error) console.error('error:', error);
+  if (error) {
+    // console.error('error:', error);
+  }
   // console.log('code:', response && response.statusCode);
   console.log(JSON.parse(body).title);
 });
